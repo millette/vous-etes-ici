b8b56f2bebc3b04b5fc4cdd7f9b7191a6c7ce972
Robin Millette <robin@millette.info>
2016-08-13 11:56:53 -0400

    Smooth scroll.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+'use strict'
+
+$(function () {
+  // console.log('\'Allo \'Allo!')
+  $('a[href*="#"]:not([href="#"])').click(function(e) {
+    $(this).animatescroll({ easing: 'easeOutBack' })
+    /*
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']')
+      if (target.length) {
+        e.preventDefault()
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600, 'linear') // 'swing' by default
+      }
+    }
+    */
+  })
+})
