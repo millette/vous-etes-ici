af8e0bee3ee42de159626e458f1eb7b9ef3ece17
Robin Millette <robin@millette.info>
2016-08-13 12:38:27 -0400

    Builds (with semi-colons...)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,10 @@
-'use strict'
+/* global $ */
+'use strict';
 
 $(function () {
   // console.log('\'Allo \'Allo!')
-  $('a[href*="#"]:not([href="#"])').click(function(e) {
-    $(this).animatescroll({ easing: 'easeOutBack' })
-    /*
-    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash)
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']')
-      if (target.length) {
-        e.preventDefault()
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 600, 'linear') // 'swing' by default
-      }
-    }
-    */
-  })
-})
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    $(this).animatescroll({ easing: 'easeOutBack' });
+  });
+});
+
